0c6eb222548bf80d7b404eb9ece83591aa095ab0
Felipe Amaral <felipeamaraldev15@gmail.com>
2023-09-17 02:13:44 -0300

    t4

--- BSI-PP-T4-Felipe-AnaCarolina-Raphael-Bruno.cs
+++ BSI-PP-T4-Felipe-AnaCarolina-Raphael-Bruno.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Threading;
 
-class T4
+public class T4
 {
-    public static SemaphoreSlim semaforoSistema = new SemaphoreSlim(1);
-    public static SemaphoreSlim semaforoSomatorio = new SemaphoreSlim(0);
     const int LINHA = 3;
 
     public static void Main(string[] args)
@@ -14,8 +12,11 @@ class T4
         int[] x = new int[LINHA];
         int i;
 
-        Thread t1 = new Thread(() => CalcularSistema(matriz, x, b));
-        Thread t2 = new Thread(() => CalcularSomatorio(matriz, x));
+        SemaphoreSlim semaforoSoma = new SemaphoreSlim(1);
+        SemaphoreSlim semaforoSistema = new SemaphoreSlim(0);
+
+        Thread t1 = new Thread(() => CalculaSoma(matriz, x, semaforoSoma, semaforoSistema));
+        Thread t2 = new Thread(() => CalculaSistema(matriz, x, b, semaforoSoma, semaforoSistema));
 
         // iniciando as threads
         t1.Start();
@@ -25,36 +26,38 @@ class T4
         t1.Join();
         t2.Join();
 
-        // imprimindo o resultado
+        Console.WriteLine("\nSolucao:");
         for (i = 0; i < LINHA; i++)
         {
-            Console.WriteLine($"X{i}: {x[i]}");
+            Console.WriteLine($"x[{i}] = {x[i]}");
         }
-
-        Console.WriteLine("Terminado \n");
     }
 
-    public static void CalcularSistema(int[,] matriz, int[] x, int[] b)
+    public static void CalculaSoma(int[,] matriz, int[] x, SemaphoreSlim semaforoSoma, SemaphoreSlim semaforoSistema)
+    {
+        int i,j,soma;
+
+        for (i = 0; i < LINHA; i++)
+        {
+            semaforoSoma.Wait();
+            soma = 0;
+            for (j = 0; j < LINHA; j++)
+            {
+                soma += matriz[i, j] * x[j];
+            }
+            x[i] = soma; // Armazena o somatório calculado
+            semaforoSistema.Release(); // Libera t2 para calcular o sistema
+        }
+    }
+
+    public static void CalculaSistema(int[,] matriz, int[] x, int[] b, SemaphoreSlim semaforoT1, SemaphoreSlim semaforoT2)
     {
         int i;
         for (i = 0; i < LINHA; i++)
         {
-            semaforoSistema.Wait(); // Aguarda permissão para cálculos
-            int somatorio = CalcularSomatorio(matriz, x, i); // Passa o i como parâmetro
-            x[i] = (b[i] - somatorio) / matriz[i, i];
-            semaforoSomatorio.Release(); // Libera a thread t2 para o próximo cálculo
+            semaforoT2.Wait();
+            x[i] = (b[i] - x[i]) / matriz[i, i]; // Calcula o sistema
+            semaforoT1.Release(); // Libera t1 para calcular o próximo somatório
         }
-    }
-
-    public static int CalcularSomatorio(int[,] matriz, int[] x, int i = 0)
-    {
-        int j;
-        int somatorio = 0;
-        for (j = 0; j < i; j++)
-        {
-            somatorio = somatorio + (matriz[i, j] * x[j]);
-        }
-        semaforoSistema.Release(); // Libera o semáforo para o cálculo da próxima linha
-        return somatorio;
     }
 }
